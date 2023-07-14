078dbe049cb04f11b1bd68ca2d2eb5b3717d51fc
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-07-14 21:47:15 +0800

    update logo

--- src/service/errorHandler.service.js
+++ src/service/errorHandler.service.js
@@ -2,6 +2,7 @@ import router from '@/router';
 
 export const hadlerError = {
   async errorHandler(err) {
+    console.log(err.message);
     if (err.message == 'Invalid token specified') {
       router.push({ name: 'Login' });
     }
